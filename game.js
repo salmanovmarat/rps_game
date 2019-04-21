c020da8843332fd9e9546a761539fd80467b82d6
Your Name <you@example.com>
2019-04-21 22:46:09 +0400

    comment

---
+++ game.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCrqKrAJDNOc3CfkTYb8yUDzB4lXBJpc8Y",
+    authDomain: "highest-bidder-708a3.firebaseapp.com",
+    databaseURL: "https://highest-bidder-708a3.firebaseio.com",
+    projectId: "highest-bidder-708a3",
+    storageBucket: "highest-bidder-708a3.appspot.com",
+    messagingSenderId: "832070231614"
+  };
+  firebase.initializeApp(config);
